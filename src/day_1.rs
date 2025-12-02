aed81d5649997a5cce38e7ad79973305d362a1f7
Kristian Coulson <kristiancoulson@gmail.com>
2025-12-02 08:32:06 +0000

    part 2 prog

--- src/day_1.rs
+++ src/day_1.rs
@@ -64,7 +64,7 @@ use std::io::{BufRead, BufReader};
 
 pub fn day_one() {
     part_one();
-    // part_two();
+    part_two();
 }
 
 pub fn part_one() {
@@ -75,8 +75,6 @@ pub fn part_one() {
         .lines()
         .map(|line| line.unwrap())
         .collect::<Vec<String>>();
-
-    println!("Rotations length: {}", rotations.len());
 
     let mut current_position = 50;
     let mut times_zero = 0;
@@ -99,7 +97,7 @@ pub fn part_one() {
         }
     }
 
-    println!("The password is: {}", times_zero);
+    println!("The part 1 password is: {}", times_zero);
 }
 
 fn turn_left(current_position: i32, distance: i32) -> i32 {
@@ -120,6 +118,114 @@ fn turn_right(current_position: i32, distance: i32) -> i32 {
     }
 
     difference
+}
+
+// You're sure that's the right password, but the door won't open. You knock, but nobody answers.
+// You build a snowman while you think. As you're rolling the snowballs for your snowman,
+// you find another security document that must have fallen into the snow:
+
+// "Due to newer security protocols, please use password method 0x434C49434B until further notice."
+
+// You remember from the training seminar that "method 0x434C49434B" means you're actually
+// supposed to count the number of times any click causes the dial to point at 0, regardless of
+// whether it happens during a rotation or at the end of one.
+
+// Following the same rotations as in the above example, the dial points at zero a few extra times during its rotations:
+
+// The dial starts by pointing at 50.
+// The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
+// The dial is rotated L30 to point at 52.
+// The dial is rotated R48 to point at 0.
+// The dial is rotated L5 to point at 95.
+// The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
+// The dial is rotated L55 to point at 0.
+// The dial is rotated L1 to point at 99.
+// The dial is rotated L99 to point at 0.
+// The dial is rotated R14 to point at 14.
+// The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+// In this example, the dial points at 0 three times at the end of a rotation, plus three more times during a rotation. So, in this example, the new password would be 6.
+
+// Be careful: if the dial were pointing at 50, a single rotation like R1000 would cause the dial to point at 0 ten times before returning back to 50!
+
+// Using password method 0x434C49434B, what is the password to open the door?
+
+pub fn part_two() {
+    let file_path = "src/day_1_input.txt";
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+    let rotations = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();;
+
+    let mut current_position = 50;
+    let mut times_zero = 0;
+
+    for rotation in rotations {
+        let (direction, turn_amount) = rotation.split_at(1);
+        let turn_amount = turn_amount.parse::<i32>().unwrap();
+
+        match direction {
+            // rem_euclid works but I wanted to use my own fn
+            // "L" => current_num = (current_num - turn_amount).rem_euclid(100),
+            // "R" => current_num = (current_num + turn_amount).rem_euclid(100),
+            "L" => {
+                let (new_position, zero_clicks) = turn_left_with_clicks(current_position, turn_amount);
+                current_position = new_position;
+                times_zero += zero_clicks;
+            }
+            "R" => {
+                let (new_position, zero_clicks) = turn_right_with_clicks(current_position, turn_amount);
+                current_position = new_position;
+                times_zero += zero_clicks;
+            }
+            _ => panic!("Invalid direction: {}", direction),
+        }
+    }
+
+    println!("The part 2 password is: {}", times_zero);
+}
+
+
+fn turn_left_with_clicks(current_position: i32, distance: i32) -> (i32, i32) {
+    let mut zero_clicks = 0;
+    let mut difference = current_position - distance;
+
+    // wrap around
+    while difference < 0 {
+        difference += 100;
+        if current_position > 0 {
+            zero_clicks += 1;
+        }
+    }
+
+    // final pos is 0
+    if difference == 0 {
+        zero_clicks += 1;
+    }
+
+
+    (difference, zero_clicks)
+}
+
+fn turn_right_with_clicks(current_position: i32, distance: i32) -> (i32, i32) {
+    let mut zero_clicks = 0;
+    let mut difference = current_position + distance;
+    let mut wrapped = false;
+
+    while difference > 99 {
+        difference -= 100;
+        wrapped = true;
+        zero_clicks += 1;
+    
+    }
+
+    // final pos is 0
+    if difference == 0 && !wrapped {
+        zero_clicks += 1;
+    }
+
+    (difference, zero_clicks)
 }
 
 #[cfg(test)]
@@ -161,4 +267,76 @@ mod tests {
         // looping multiple times]
         assert_eq!(turn_right(14, 200), 14);
     }
+
+    
+    #[test]
+    fn test_turn_left_with_clicks() {
+        assert_eq!(turn_left_with_clicks(50, 10), (40, 0));
+        // starting on 0
+        assert_eq!(turn_left_with_clicks(0, 10), (90, 0), "starting on 0");
+        // landing on 0
+        assert_eq!(turn_left_with_clicks(0, 1), (0, 1), "landing on 0");
+        // landing on 0 after wrapping
+        assert_eq!(turn_left_with_clicks(0, 101), (0, 2), "landing on 0 after wrapping");
+        // wrapping around multiple times
+        assert_eq!(turn_left_with_clicks(10, 1000), (10, 10), "wrapping around multiple times");
+        // starting on 0
+        assert_eq!(turn_left_with_clicks(0, 10), (10, 0), "starting on 0");
+        // starting on 0 after wrapping
+        assert_eq!(turn_left_with_clicks(0, 101), (1, 1), "starting on 0 after wrapping");
+        // wrapping around multiple times
+        assert_eq!(turn_left_with_clicks(0, 1000), (0, 10), "wrapping around multiple times");
+    }
+
+    #[test]
+    fn test_turn_right_with_clicks() {
+        assert_eq!(turn_right_with_clicks(50, 10), (60, 0));
+        // looping around once
+        assert_eq!(turn_right_with_clicks(99, 10), (9, 1), "looping around once");
+        // landing on 0
+        assert_eq!(turn_right_with_clicks(99, 1), (0, 1), "landing on 0");
+        // landing on 0 after wrapping
+        assert_eq!(turn_right_with_clicks(99, 101), (0, 2), "landing on 0 after wrapping");
+        // wrapping around multiple times
+        assert_eq!(turn_right_with_clicks(10, 1000), (10, 10), "wrapping around multiple times");
+        // starting on 0
+        assert_eq!(turn_right_with_clicks(0, 10), (10, 0), "starting on 0");
+        // starting on 0 after wrapping
+        assert_eq!(turn_right_with_clicks(0, 101), (1, 1), "starting on 0 after wrapping");
+        // wrapping around multiple times
+        assert_eq!(turn_right_with_clicks(0, 1000), (0, 10), "wrapping around multiple times");
+    }
+
+    // #[test]
+    // fn test_day_two() {
+    //     // The dial starts by pointing at 50.
+    //     // The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
+    //     // The dial is rotated L30 to point at 52.
+    //     // The dial is rotated R48 to point at 0.
+    //     // The dial is rotated L5 to point at 95.
+    //     // The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
+    //     // The dial is rotated L55 to point at 0.
+    //     // The dial is rotated L1 to point at 99.
+    //     // The dial is rotated L99 to point at 0.
+    //     // The dial is rotated R14 to point at 14.
+    //     // The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+    //     assert_eq!(turn_left_with_clicks(50, 68), (82, 1), "The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.");
+    //     assert_eq!(turn_left_with_clicks(82, 30), (52, 0), "The dial is rotated L30 to point at 52.");
+    //     assert_eq!(turn_right_with_clicks(52, 48), (0, 1), "The dial is rotated R48 to point at 0.");
+    //     assert_eq!(turn_left_with_clicks(0, 5), (95, 0), "The dial is rotated L5 to point at 95.");
+    //     assert_eq!(turn_right_with_clicks(95, 60), (55, 1), "The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.");
+    //     assert_eq!(turn_left_with_clicks(55, 55), (0, 1), "The dial is rotated L55 to point at 0.");
+    //     assert_eq!(turn_left_with_clicks(0, 1), (99, 0), "The dial is rotated L1 to point at 99.");
+    //     assert_eq!(turn_left_with_clicks(99, 99), (0, 1), "The dial is rotated L99 to point at 0.");
+    //     assert_eq!(turn_right_with_clicks(0, 14), (14, 0), "The dial is rotated R14 to point at 14.");
+    //     assert_eq!(turn_left_with_clicks(14, 82), (32, 1), "The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.");
+        
+        
+    //     // checking wrapping logic
+    //     assert_eq!(turn_left_with_clicks(50, 1000), (50, 10), "The dial is rotated L1000 to point at 50.");
+    //     assert_eq!(turn_left_with_clicks(0, 1000), (0, 10), "The dial is rotated L1000 to point at 50.");
+
+    // }
+
+    
 }
